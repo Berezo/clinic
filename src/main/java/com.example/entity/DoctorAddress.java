c6b061165b1eca4544649073f61cb6069eea5d34
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-04 14:05:02 +0200

    Uzupełniono klasy związane z lekarzem.

--- src/main/java/com.example/entity/DoctorAddress.java
+++ src/main/java/com.example/entity/DoctorAddress.java
@@ -1,4 +1,86 @@
 package com.example.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "doctor_address")
 public class DoctorAddress {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Id
+    private int id;
+
+    @Column(name = "house_number")
+    private String house_number;
+
+    @Column(name="street")
+    private String street;
+
+    @Column(name="city")
+    private String city;
+
+    @Column(name="zip_code")
+    private String zip_code;
+
+    public DoctorAddress() {
+    }
+
+    public DoctorAddress(String house_number, String street, String city, String zip_code) {
+        this.house_number = house_number;
+        this.street = street;
+        this.city = city;
+        this.zip_code = zip_code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHouse_number() {
+        return house_number;
+    }
+
+    public void setHouse_number(String house_number) {
+        this.house_number = house_number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    @Override
+    public String toString() {
+        return "PatientAddress{" +
+                "id=" + id +
+                ", house_number='" + house_number + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", zip_code='" + zip_code + '\'' +
+                '}';
+    }
 }
--- src/main/java/com.example/entity/OfficeHours.java
+++ src/main/java/com.example/entity/OfficeHours.java
@@ -1,4 +1,73 @@
 package com.example.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="office_hours")
 public class OfficeHours {
+
+    @Column(name = "id")
+    @Id
+    private int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="doctor_id")
+    private Doctor doctor;
+
+    @Column(name = "day")
+    private String day;
+
+    @Column(name = "hour")
+    private String hour;
+
+    public OfficeHours() {
+    }
+
+    public OfficeHours(Doctor doctor, String day, String hour) {
+        this.doctor = doctor;
+        this.day = day;
+        this.hour = hour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    @Override
+    public String toString() {
+        return "OfficeHours{" +
+                "id=" + id +
+                ", doctor=" + doctor +
+                ", day='" + day + '\'' +
+                ", hour='" + hour + '\'' +
+                '}';
+    }
 }
