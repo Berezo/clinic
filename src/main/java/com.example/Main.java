e29fd00b2f8a81a7b851aa3984bc6b8e2014a508
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-04 12:35:19 +0200

    Alpha połączenie z bazą danych.

--- src/main/java/com.example/Main.java
+++ src/main/java/com.example/Main.java
@@ -1,8 +1,30 @@
 package com.example;
 
+import com.example.entity.Patient;
+import com.example.entity.PatientAddress;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("dupa");
-        System.out.println("develop");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.xml")
+                .addAnnotatedClass(PatientAddress.class)
+                .addAnnotatedClass(Patient.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<Patient> patients = session.createQuery("SELECT a FROM Patient a", Patient.class).getResultList();
+
+        for(Patient patient:patients){
+            System.out.println(patient);
+        }
+        session.getTransaction().commit();
     }
 }
---
+++ src/main/java/com.example/entity/Patient.java
@@ -0,0 +1,72 @@
+package com.example.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="patient")
+public class Patient {
+
+    @Column(name="id")
+    @Id
+    private int id;
+
+    @Column(name="first_name")
+    private String first_name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="address_id")
+    private PatientAddress address;
+
+    public Patient() {
+    }
+
+    public Patient(String first_name, String surname, PatientAddress address) {
+        this.first_name = first_name;
+        this.surname = surname;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public PatientAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(PatientAddress address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "first_name='" + first_name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
