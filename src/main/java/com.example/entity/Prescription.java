0b893431693944c94b0a6ebbd27fff93a64222e9
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-05 13:12:26 +0200

    Dodano klasę recepty.

---
+++ src/main/java/com.example/entity/Prescription.java
@@ -0,0 +1,87 @@
+package com.example.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "prescription")
+public class Prescription {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Id
+    private int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="patient_id")
+    private Patient patient;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="doctor_id")
+    private Doctor doctor;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "medicines")
+    private String medicines;
+
+    public Prescription() {
+    }
+
+    public Prescription(Patient patient, Doctor doctor, String description, String medicines) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.description = description;
+        this.medicines = medicines;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMedicines() {
+        return medicines;
+    }
+
+    public void setMedicines(String medicines) {
+        this.medicines = medicines;
+    }
+
+    @Override
+    public String toString() {
+        return "Prescription{" +
+                "patient=" + patient +
+                ", doctor=" + doctor +
+                ", description='" + description + '\'' +
+                ", medicines='" + medicines + '\'' +
+                '}';
+    }
+}
