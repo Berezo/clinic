de280a20053252a0238c5487cdea496e34edb2c4
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-06 16:12:19 +0200

    Kilka poprawek w skypcie bazy. - zmiana typu house_number z int na varchar. - usuniecie kilku NOT NULL. - zmiana hour na start_hour i end_hour w tabeli office_hours. - dodanie doctor_description do tabeli visit.

--- src/main/resources/skypt_bazy_danych.sql
+++ src/main/resources/skypt_bazy_danych.sql
@@ -5,7 +5,7 @@
 -- GRANT ALL PRIVILEGES ON * . * TO 'clinic_admin'@'localhost';
 
 -- USE clinic;
-
+--
 -- DROP TABLE IF EXISTS visit;
 -- DROP TABLE IF EXISTS prescription;
 -- DROP TABLE IF EXISTS patient;
@@ -13,17 +13,17 @@
 -- DROP TABLE IF EXISTS office_hours;
 -- DROP TABLE IF EXISTS doctor;
 -- DROP TABLE IF EXISTS doctor_address;
-
-
+--
+--
 -- CREATE TABLE `patient_address`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
---     `house_number` INT,
+--     `house_number` VARCHAR(10),
 --     `street` VARCHAR(50),
 --     `city` VARCHAR(50),
 --     `zip_code` VARCHAR(7),
 --     CONSTRAINT `patient_address_pk` PRIMARY KEY(`id`)
 --     );
---     
+--
 -- CREATE TABLE `patient`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
 --     `first_name` VARCHAR(50) NOT NULL,
@@ -32,16 +32,16 @@
 --     CONSTRAINT `patient_pk` PRIMARY KEY(`id`),
 --     CONSTRAINT `patient_address_fk` FOREIGN KEY(`address_id`) REFERENCES `patient_address`(`id`) ON DELETE SET NULL
 --     );
-
+--
 -- CREATE TABLE `doctor_address`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
---     `house_number` INT,
+--     `house_number` VARCHAR(10),
 --     `street` VARCHAR(50),
 --     `city` VARCHAR(50),
 --     `zip_code` VARCHAR(7),
 --     CONSTRAINT `doctor_address_pk` PRIMARY KEY(`id`)
 --     );
---     
+--
 -- CREATE TABLE `doctor`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
 --     `first_name` VARCHAR(50) NOT NULL,
@@ -51,7 +51,7 @@
 --     CONSTRAINT `doctor_pk` PRIMARY KEY(`id`),
 --     CONSTRAINT `doctor_address_fk` FOREIGN KEY(`address_id`) REFERENCES `doctor_address`(`id`) ON DELETE SET NULL
 --     );
---     
+--
 -- CREATE TABLE `prescription`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
 --     `patient_id` INT,
@@ -62,34 +62,36 @@
 --     CONSTRAINT `prescription_doctor_fk` FOREIGN KEY(`doctor_id`) REFERENCES `doctor`(`id`) ON DELETE SET NULL,
 --     CONSTRAINT `prescription_patient_fk` FOREIGN KEY(`patient_id`) REFERENCES `patient`(`id`) ON DELETE SET NULL
 --     );
---     
+--
 -- CREATE TABLE `visit`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
 --     `patient_id` INT,
 --     `doctor_id` INT,
 --     `is_examination` BOOLEAN NOT NULL,
---     `patient_description` VARCHAR(200) NOT NULL,
+--     `patient_description` VARCHAR(200),
+--     `doctor_description` VARCHAR(200),
 --     `prescription_id` INT,
 --     `registration_date` DATE NOT NULL,
 --     `visit_date` DATETIME NOT NULL,
---     `visit_made` BOOLEAN NOT NULL,
+--     `visit_made` BOOLEAN,
 --     `cancel_cause` VARCHAR(200),
 --     CONSTRAINT `visit_pk` PRIMARY KEY(`id`),
 --     CONSTRAINT `visit_patient_fk` FOREIGN KEY(`patient_id`) REFERENCES `patient`(`id`) ON DELETE SET NULL,
 --     CONSTRAINT `visit_doctor_fk` FOREIGN KEY(`doctor_id`) REFERENCES `doctor`(`id`) ON DELETE SET NULL,
 --     CONSTRAINT `visit_prescription_fk` FOREIGN KEY(`prescription_id`) REFERENCES `prescription`(`id`) ON DELETE SET NULL
 --     );
---     
+--
 -- CREATE TABLE `office_hours`(
 -- 	`id` INT AUTO_INCREMENT NOT NULL,
 --     `doctor_id` INT,
 --     `day` VARCHAR(20) NOT NULL,
---     `hour` TIME NOT NULL,
+--     `start_hour` TIME NOT NULL,
+--     `end_hour` TIME NOT NULL,
 --     CONSTRAINT `office_hours_pk` PRIMARY KEY(`id`),
 --     CONSTRAINT `office_hourst_doctor_fk` FOREIGN KEY(`doctor_id`) REFERENCES `doctor`(`id`) ON DELETE SET NULL
 --     );
---     
---     
+--
+--
 -- INSERT INTO `patient_address`(`house_number`, `street`, `city`, `zip_code`) VALUES ( 13, 'test', 'test_city', '11-111');
 -- INSERT INTO `patient`(`first_name`, `surname`, `address_id`) VALUES ('test_name', 'test_surname', 1);
 
@@ -101,6 +103,5 @@
 -- SELECT * FROM patient_address;
 -- SELECT * FROM patient;
 
-
 SELECT * FROM doctor JOIN doctor_address ON doctor.address_id = doctor_address.id;
-SELECT * FROM doctor;
+-- SELECT * FROM doctor;
