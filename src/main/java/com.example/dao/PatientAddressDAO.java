eb090f926935399710c9ebedfdd6e9d1b24b038a
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-15 12:10:26 +0200

    PatientAddressDAO

---
+++ src/main/java/com.example/dao/PatientAddressDAO.java
@@ -0,0 +1,32 @@
+package com.example.dao;
+
+import com.example.entity.PatientAddress;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PatientAddressDAO {
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<PatientAddress> getPatientAddresses(){
+        Session session = sessionFactory.getCurrentSession();
+        String queryString = "SELECT a FROM PatientAddress a";
+        Query<PatientAddress> query = session.createQuery(queryString,PatientAddress.class);
+        return query.getResultList();
+    }
+
+    public PatientAddress getPatientAddress(int id){
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(PatientAddress.class, id);
+    }
+}
