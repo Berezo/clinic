b742d1ce1adc21a8dbb2e8b53124d1219ae3276e
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-15 12:43:41 +0200

    PatientDAO.

---
+++ src/main/java/com.example/dao/PatientDAO.java
@@ -0,0 +1,33 @@
+package com.example.dao;
+
+import com.example.entity.Doctor;
+import com.example.entity.Patient;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PatientDAO {
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Patient> getPatients(){
+        Session session = sessionFactory.getCurrentSession();
+        String queryString = "SELECT p FROM Patient p";
+        Query<Patient> query = session.createQuery(queryString, Patient.class);
+        return query.getResultList();
+    }
+
+    public Patient getPatient(int id){
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Patient.class, id);
+    }
+}
