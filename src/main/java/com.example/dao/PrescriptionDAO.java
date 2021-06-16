97c4706c4c65cbcf6bf809256db94bb0249cad8d
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-16 15:26:32 +0200

    Niezbędne klasy dla Prescription. PrescriptionDAO PrescriptionService PrescriptionRestController

---
+++ src/main/java/com.example/dao/PrescriptionDAO.java
@@ -0,0 +1,32 @@
+package com.example.dao;
+
+import com.example.entity.Prescription;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PrescriptionDAO {
+    SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Prescription> getPrescriptions(){
+        Session session = sessionFactory.getCurrentSession();
+        String queryString = "SELECT p FROM Prescription p";
+        Query<Prescription> query = session.createQuery(queryString,Prescription.class );
+        return query.getResultList();
+    }
+
+    public Prescription getPrescription(int id){
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Prescription.class, id);
+    }
+}
