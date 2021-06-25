e858a5306303a07c7a786332b2630d46db2d43f8
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-25 12:30:50 +0200

    Klasa OfficeHoursController do obsługi zwykłych widoków.

---
+++ src/main/java/com.example/controller/OfficeHoursController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import com.example.entity.OfficeHours;
+import com.example.service.OfficeHoursService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/office-hours")
+public class OfficeHoursController {
+    private OfficeHoursService officeHoursService;
+
+    @Autowired
+    public void setOfficeHoursService(OfficeHoursService officeHoursService) {
+        this.officeHoursService = officeHoursService;
+    }
+
+    @GetMapping("/")
+    public String OfficeHoursList(Model model){
+        List<OfficeHours> officeHours = officeHoursService.getOfficeHours();
+        model.addAttribute("officeHours", officeHours);
+        return "officehourslist";
+    }
+}
