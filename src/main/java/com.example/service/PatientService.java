58a979cf469304e5287099fde3702f4c12e275e3
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-25 22:52:16 +0200

    Usunięte niepotrzebne pole passwordEncoder.

--- src/main/java/com.example/service/PatientService.java
+++ src/main/java/com.example/service/PatientService.java
@@ -5,7 +5,6 @@ import com.example.dao.PatientDAO;
 import com.example.entity.Patient;
 import com.example.entity.PatientAddress;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,9 +15,6 @@ public class PatientService {
 
     @Autowired
     private PatientDAO patientDAO;
-
-    @Autowired
-    private  PasswordEncoder passwordEncoder;
 
     @Autowired
     private PatientAddressDAO patientAddressDAO;
