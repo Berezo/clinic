2b6c4f13cd34451955ef7617ff6a262da6572ec8
Krystian Janasz <krystian.janasz@gmail.com>
2021-06-25 22:51:09 +0200

    Działające rejestracja jedynia, działa tylko dla pacjenta.

--- src/main/java/com.example/controller/HomeController.java
+++ src/main/java/com.example/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
 
+import com.example.entity.Authority;
 import com.example.entity.Patient;
+import com.example.entity.User;
 import com.example.service.PatientService;
+import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class HomeController {
 
+    private UserService userService;
+
     private PatientService patientService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @Autowired
     public void setPatientService(PatientService patientService) {
@@ -27,8 +37,9 @@ public class HomeController {
 
     @GetMapping(value = "/register")
     public String registerForm(Model model) {
-        model.addAttribute("user", new Patient());
-        return "patientregister";
+        model.addAttribute("user", new User());
+        model.addAttribute("patient", new Patient());
+        return "register";
     }
 
     @PostMapping("/register")
@@ -36,15 +47,20 @@ public class HomeController {
         String validator = validate(user);
         if (!validator.isEmpty()) {
             model.addAttribute("validator", validator);
-            return "patientregister";
+            return "register";
         }
+        user.setAuthorities(new Authority(user, "ROLE_PATIENT"));
+        userService.saveUser(user);
         return "redirect:/login";
     }
 
-    private String validate(Patient user) {
-        if (patientService.getPatient(user.getId()) != null) {
+    private String validate(User user) {
+        if (userService.getUser(user.getUsername()) != null) {
             return "użytkownik o takiej nazwie istnieje";
-        } else if (user.getLogin().isEmpty() || user.getPassword().isEmpty())
+        } else if (user.getUsername().isEmpty() || user.getPassword().isEmpty())
+        {
+            return "wypełnij wszystkie pola";
+        } else if (user.getPatient().getFirst_name().isEmpty() || user.getPatient().getSurname().isEmpty())
         {
             return "wypełnij wszystkie pola";
         }
